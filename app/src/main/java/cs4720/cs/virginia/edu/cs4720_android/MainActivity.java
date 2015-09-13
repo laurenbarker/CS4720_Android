7ff5b0b633a28afb422e692cff87878ac0895a3d
Reina Hernandez <reinaH@d-172-25-88-41.bootp.Virginia.EDU>
2015-09-13 12:26:11 -0400

    modified project

--- app/src/main/java/cs4720/cs/virginia/edu/cs4720_android/MainActivity.java
+++ app/src/main/java/cs4720/cs/virginia/edu/cs4720_android/MainActivity.java
@@ -1,11 +1,16 @@
 package cs4720.cs.virginia.edu.cs4720_android;
 
+
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +38,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void startService(View view) {
+        Intent intent = new Intent(this, GetCurrentLocation.class);
+        startService(intent);
+    }
+
+    public void stopService(View view) {
+        Intent intent = new Intent(this, GetCurrentLocation.class);
+        stopService(intent);
     }
 }
